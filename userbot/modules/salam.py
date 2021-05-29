9de00bdd6a183bfdeba61b00be41b2db3aa15946
adudon01 <85031696+adudon01@users.noreply.github.com>
2021-05-30 02:45:48 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -1,3 +1,5 @@
+//adudin
+from time import sleep
 from platform import uname
 from userbot import ALIVE_NAME, CMD_HELP
 from userbot.events import register
@@ -7,34 +9,55 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^.P(?: |$)(.*)')
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb kawan Adudin.`")
+    sleep(1)
+    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER}**")
+    sleep(2)
+    await typew.edit("`Assalamualaikum.....`")
+# Owner @Si_Dian
+# Thanks XBOT-REMIX
 
-
-@register(outgoing=True, pattern='^.p(?: |$)(.*)')
+@register(outgoing=True, pattern='^p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb kawan Adudin.`")
+    sleep(1)
+    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER}**")
+    sleep(2)
+    await typew.edit("`Assalamualaikum.....`")
+# Owner @Si_Dian
+# Izin Maling Om
 
-
-@register(outgoing=True, pattern='^.L(?: |$)(.*)')
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wa'alaikumssalam wr. wb kawan.`")
+    sleep(1)
+    await typew.edit("`Astaghfirulloh lupa...`")
+    sleep(1)
+    await typew.edit(f"**{DEFAULTUSER} mengucapkan**")
+    sleep(1)
+    await typew.edit("`Waallaikumsalam......`")
+# Owner @Si_Dian
+# Izin Maling Om
 
-
-@register(outgoing=True, pattern='^.l(?: |$)(.*)')
+@register(outgoing=True, pattern='^l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wa'alaikumssalam wr. wb kawan.`")
-
+    sleep(1)
+    await typew.edit("`Astaghfirulloh lupa...`")
+    sleep(1)
+    await typew.edit(f"**{DEFAULTUSER} mengucapkan**")
+    sleep(1)
+    await typew.edit("`Waallaikumsalam......`")
+# Owner @Si_Dian
+# Izin Maling Om
+# Hehehehe
 
 CMD_HELP.update({
     "salam":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.P` | `.p`\
-\n↳ : Untuk Memberi salam.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.L` `.l`\
-\n↳ : Untuk Menjawab Salam."
+    "`P`\
+\nUsage: Untuk Memberi salam.\
+\n\n`L`\
+\nUsage: Untuk Menjawab Salam."
 })
