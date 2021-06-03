e8a702bac2aff26f4cf9c34c02158814ee072fbb
Adudin <85031696+adudon01@users.noreply.github.com>
2021-06-03 15:42:51 +0700

    Create faudio.py

---
+++ userbot/modules/faudio.py
@@ -0,0 +1,16 @@
+@ultroid_cmd(pattern="faudio ?(.*)")
+async def _(e):
+    t = e.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(e, t)
+            except BaseException:
+                return await eod(e, "`Incorrect Format`")
+    await eod(e, f"Starting Fake audio recording For {t} sec.")
+    async with e.client.action(e.chat_id, "record-audio"):
+        await asyncio.sleep(t)
