f88845d132709188d497406bb4033ef50013f607
yud023 <83487202+yud023@users.noreply.github.com>
2021-06-05 02:37:29 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -53,7 +53,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await event.edit(
                 '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
-                ' untuk dapat deploy perubahan terbaru dari ⚡Geez-Project⚡.`'
+                ' untuk dapat deploy perubahan terbaru dari ⚡REBELLIONS-UserBot⚡.`'
             )
             repo.__del__()
             return
@@ -67,7 +67,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         await event.edit('`[HEROKU]:'
-                         '\nSedang MengUpdate ⚡Geez-ProjectBot⚡, Mohon Menunggu 5-7 Menit`'
+                         '\nSedang MengUpdate ⚡REBELLIONS-UserBot⚡, Mohon Menunggu 5-7 Menit`'
                          )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
@@ -115,9 +115,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**⚡Geez-Project⚡** `Berhasil Di Update!`')
+    await event.edit('**⚡REBELLIONS-UserBot⚡** `Berhasil Di Update!`')
     await asyncio.sleep(1)
-    await event.edit('**⚡Geez-Project⚡** `Di Restart....`')
+    await event.edit('**⚡REBELLIONS-UserBot⚡** `Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Mohon Menunggu Beberapa Detik.`')
     await asyncio.sleep(10)
@@ -126,7 +126,7 @@ async def update(event, repo, ups_rem, ac_br):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID, "#BOT \n"
-            "**⚡Geez-Project⚡ Telah Di Perbarui.**")
+            "**⚡REBELLIONS-UserBot⚡ Telah Di Perbarui.**")
         await asyncio.sleep(100)
         await event.delete()
 
@@ -188,13 +188,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n⚡Geez-Project⚡ Sudah Versi Terbaru\n')
+            f'\n⚡REBELLIONS-UserBot⚡ Sudah Versi Terbaru\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk ⚡Geez-Project⚡ :\n\n💻 Pembaruan Data :**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk ⚡REBELLIOND-UserBot⚡ :\n\n💻 Pembaruan Data :**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
@@ -214,12 +214,12 @@ async def upstream(event):
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('` Proses Update ⚡Geez-Project, Loading....1%`')
-        await event.edit('` Proses Update ⚡Geez-Project, Loading....20%`')
-        await event.edit('` Proses Update ⚡Geez-Project, Loading....35%`')
-        await event.edit('` Proses Update ⚡Geez-Project, Loading....77%`')
-        await event.edit('` Proses Update ⚡Geez-Project, Updating...90%`')
-        await event.edit('` Proses Update ⚡Geez-Project, Mohon Tunggu Sebentar....100%`')
+        await event.edit('` Proses Update ⚡REBELLIONS-UserBot, Loading....1%`')
+        await event.edit('` Proses Update ⚡REBELLIONS-UserBot, Loading....20%`')
+        await event.edit('` Proses Update ⚡REBELLIONS-UserBot, Loading....35%`')
+        await event.edit('` Proses Update ⚡REBELLIONS-UserBot, Loading....77%`')
+        await event.edit('` Proses Update ⚡REBELLIONS-UserBot, Updating...90%`')
+        await event.edit('` Proses Update ⚡REBELLIONS-UserBot, Mohon Tunggu Sebentar....100%`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)
@@ -234,9 +234,9 @@ async def upstream(event):
 CMD_HELP.update({
     'update':
     "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.update`"
-    "\n• : Untuk Melihat Pembaruan Terbaru Geez-Project."
+    "\n• : Untuk Melihat Pembaruan Terbaru REBELLIONS-UserBot."
     "\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.update now`"
-    "\n• : Memperbarui Geez-Project."
+    "\n• : Memperbarui REBELLIONS-UserBot."
     "\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.update deploy`"
-    "\n• : Memperbarui Geez-Project Dengan Cara Men-Deploy Ulang."
+    "\n• : Memperbarui REBELLIONS-UserBot Dengan Cara Men-Deploy Ulang."
 })
