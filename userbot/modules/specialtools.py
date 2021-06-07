342123f7acb8177199af7aed3c424cbe9840dbec
yud023 <83487202+yud023@users.noreply.github.com>
2021-06-07 13:33:23 +0700

    Update specialtools.py

--- userbot/modules/specialtools.py
+++ userbot/modules/specialtools.py
@@ -4,21 +4,11 @@
 # Credits © Geez - Projects
 
 import os
-from datetime import datetime as dt
-from random import choice
-from shutil import rmtree
 
 import moviepy.editor as m
-import pytz
-import requests
-from bs4 import BeautifulSoup as b
 
 from userbot import CMD_HELP
 from userbot.events import register
-
-
-@register(outgoing=True, pattern="^.getaudio(?: |$)(.*)")
-async def daudtoid(event):
 
 
 @register(outgoing=True, pattern="^.getaudio(?: |$)(.*)", disable_errors=True)
@@ -26,14 +16,12 @@ async def _(event):
     ureply = await event.get_reply_message()
     if not (ureply and ("audio" in ureply.document.mime_type)):
         await event.edit("`Reply To Audio Only..`")
-
-
-@ @ -25, 8 + 24, 8 @ @ async def daudtoid(event):
+        return
+    await event.edit("`processing...`")
+    d = os.path.join("resources/extras", "ul.mp3")
+    await event.edit("`Downloading... Large Files Takes Time..`")
+    await event.client.download_media(ureply, d)
     await event.edit("`Done.. Now reply to video In which u want to add that Audio`")
-
-
-@register(outgoing=True, pattern="^.addaudio(?: |$)(.*)")
-async def adaudroid(event):
 
 
 @register(outgoing=True, pattern="^.addaudio(?: |$)(.*)", disable_errors=True)
@@ -41,13 +29,35 @@ async def _(event):
     ureply = await event.get_reply_message()
     if not (ureply and ("video" in ureply.document.mime_type)):
         await event.edit("`Reply To Gif/Video In which u want to add audio.`")
-
-
-@ @ -54, 7 + 53, 6 @ @ async def adaudroid(event):
+        return
+    xx = await event.edit("`processing...`")
+    ultt = await ureply.download_media()
+    ls = os.listdir("resources/extras")
+    z = "ul.mp3"
+    x = "resources/extras/ul.mp3"
+    if z not in ls:
+        await event.edit("`First reply an audio with .aw`")
+        return
+    video = m.VideoFileClip(ultt)
+    audio = m.AudioFileClip(x)
+    out = video.set_audio(audio)
+    out.write_videofile("ok.mp4", fps=30)
+    await event.client.send_file(
+        event.chat_id,
+        file="ok.mp4",
+        force_document=False,
+        reply_to=event.reply_to_msg_id,
+    )
+    os.remove("ok.mp4")
+    os.remove(x)
     os.remove(ultt)
     await xx.delete()
-
 
 CMD_HELP.update(
     {
         "specialtools": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.getaudio`\
+         \n↳ : Download Audio To put in ur Desired Video/Gif..\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.addaudio`\
+         \n↳ : It will put the above audio to the replied video/gif.."
+    }
+)
