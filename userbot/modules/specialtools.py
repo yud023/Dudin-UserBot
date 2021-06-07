c1a669805c9b83789659b489016d796acde55868
Adudin <Dusep21@gmail.com>
2021-06-07 06:28:39 +0000

    pylint: auto fixes
    Signed-off-by: Adudin <dusep21@gmail.com>

--- userbot/modules/specialtools.py
+++ userbot/modules/specialtools.py
@@ -19,23 +19,31 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.getaudio(?: |$)(.*)")
 async def daudtoid(event):
+
+
 @register(outgoing=True, pattern="^.getaudio(?: |$)(.*)", disable_errors=True)
 async def _(event):
     ureply = await event.get_reply_message()
     if not (ureply and ("audio" in ureply.document.mime_type)):
         await event.edit("`Reply To Audio Only..`")
-@@ -25,8 +24,8 @@ async def daudtoid(event):
+
+
+@ @ -25, 8 + 24, 8 @ @ async def daudtoid(event):
     await event.edit("`Done.. Now reply to video In which u want to add that Audio`")
 
 
 @register(outgoing=True, pattern="^.addaudio(?: |$)(.*)")
 async def adaudroid(event):
+
+
 @register(outgoing=True, pattern="^.addaudio(?: |$)(.*)", disable_errors=True)
 async def _(event):
     ureply = await event.get_reply_message()
     if not (ureply and ("video" in ureply.document.mime_type)):
         await event.edit("`Reply To Gif/Video In which u want to add audio.`")
-@@ -54,7 +53,6 @@ async def adaudroid(event):
+
+
+@ @ -54, 7 + 53, 6 @ @ async def adaudroid(event):
     os.remove(ultt)
     await xx.delete()
 
