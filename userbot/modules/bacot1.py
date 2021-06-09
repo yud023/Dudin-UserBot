9fd72aa8ca244e92009e089e1500725616c16fa3
yud023 <83487202+yud023@users.noreply.github.com>
2021-06-09 15:21:53 +0700

    Update bacot1.py

--- userbot/modules/bacot1.py
+++ userbot/modules/bacot1.py
@@ -9,53 +9,53 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^.gjn(?: |$)(.*)')
+@register(outgoing=True, pattern='^gjn(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("Gajelas Ngentottt")
 
 
-@register(outgoing=True, pattern='^.yb(?: |$)(.*)')
+@register(outgoing=True, pattern='^yb(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**Yabenarrrrrrr...**")
 
 
-@register(outgoing=True, pattern='^.gjb(?: |$)(.*)')
+@register(outgoing=True, pattern='^gjb(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**GAJELAS BABI....**")
 
 
-@register(outgoing=True, pattern='^.gjk(?: |$)(.*)')
+@register(outgoing=True, pattern='^gjk(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**Gajelas Kontolll....**")
 
 
-@register(outgoing=True, pattern='^.gbgn(?: |$)(.*)')
+@register(outgoing=True, pattern='^gbgn(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**Ga banget, Ngentott!!!**")
 
 
-@register(outgoing=True, pattern='^.gls(?: |$)(.*)')
+@register(outgoing=True, pattern='^gls(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**GAK, LO SANGEAN!!!**")
 
 CMD_HELP.update({
     "bacot1":
-    ".gjn\
+    "gjn\
 \nUsage:\
-\n\n.yb\
+\n\n`yb`\
 \nUsage:\
-\n\n.gjb\
+\n\n`gjb`\
 \nUsage:\
-\n\n.gjk\
+\n\n`gjk`\
 \nUsage:\
-\n\n.gbgn\
+\n\n`gbgn`\
 \nUsage:\
-\n\n.gls\
+\n\n`gls`\
 \nUsage:"
 })
