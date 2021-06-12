00f40ae795bb0ab0cee1692247284b7bb36464a4
yud023 <83487202+yud023@users.noreply.github.com>
2021-06-12 17:48:08 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -18,7 +18,7 @@ async def lastname(steal):
     if message.sender.bot:
         await steal.edit("```Balas Ke Pesan Pengguna Yang Sebenarnya.```")
         return
-    await steal.edit("```Mengambil Informasi Pengguna Tersebut, Mohon Menunggu..```")
+    await steal.edit("```Sedang Mengambil Data Si Babu, Mohon Menunggu..```")
     try:
         async with bot.conversation(chat) as conv:
             try:
