d41e7c918e605caffb0efb80e95c32c0eaa7ade0
yud023 <83487202+yud023@users.noreply.github.com>
2021-06-12 17:49:32 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -40,7 +40,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```Saya Tidak Menemukan Informasi Pengguna Ini, Pengguna Ini Belum Pernah Mengganti Nama Sebelumnya```")
+                await steal.edit("```Saya Tidak Menemukan Informasi Si Babu, Babu Ini Belum Pernah Mengganti Nama Sebelumnya```")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
