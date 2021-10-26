17784a45ae5707f01675f81fde95ecf98d06fddc
yud023 <83487202+yud023@users.noreply.github.com>
2021-10-26 12:37:38 +0700

    Create tak.py

---
+++ userbot/modules/tak.py
@@ -0,0 +1,49 @@
+# Port By @Yud_023
+# Yud - Userbot 
+
+
+from telethon.errors import ChatSendInlineForbiddenError, ChatSendStickersForbiddenError
+
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+
+@register(outgoing=True, pattern=r"^\.frog (.*)")
+async def honkasays(event):
+    await event.edit("`Sedang Memprosess!!!`")
+    text = event.pattern_match.group(1)
+    if not text:
+        return await event.edit("`Beri Aku Bebeberapa Teks, Contoh .frog test`")
+    try:
+        if not text.endswith("."):
+            text = text + "."
+        if len(text) <= 9:
+            results = await bot.inline_query("honka_says_bot", text)
+            await results[2].click(
+                event.chat_id,
+                silent=True,
+                hide_via=True,
+            )
+        elif len(text) >= 14:
+            results = await bot.inline_query("honka_says_bot", text)
+            await results[0].click(
+                event.chat_id,
+                silent=True,
+                hide_via=True,
+            )
+        else:
+            results = await bot.inline_query("honka_says_bot", text)
+            await results[1].click(
+                event.chat_id,
+                silent=True,
+                hide_via=True,
+            )
+        await event.delete()
+    except ChatSendInlineForbiddenError:
+        await event.edit("`Maaf Saya tidak bisa menggunakan hal-hal sebaris di sini...`")
+    except ChatSendStickersForbiddenError:
+        await event.edit("Maaf saya tidak bisa mengirim stiker ke sini !!")
+
+
+CMD_HELP.update({"frog": "**Syntax**: .frog\
+    \n**Function**: .frog <kata kata>."})
